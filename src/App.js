3d8e410876b6d252e0b7cec27b66e673c28118aa
Elvan H <elvanhd@gmail.com>
2021-12-02 23:44:03 +0700

    using the useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import MainHeader from './components/MainHeader/MainHeader';
 
+const LOCAL_STORAGE_KEY = 'dummy-auth-app-react-is-logged-in';
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
+  useEffect(() => {
+    const storedUserLoggedIn = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+    if (storedUserLoggedIn === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (_email, _password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem(LOCAL_STORAGE_KEY, '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
     setIsLoggedIn(false);
   };
 
